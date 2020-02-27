a7a21491e5ea848d42d1c223882228121812c46f
sumansaurabh47 <sumansaurabh47@users.noreply.github.com>
2020-02-27 10:29:06 +0530

    Add files via upload

--- sso-lan/src/main/java/com/SSOController.java
+++ sso-lan/src/main/java/com/SSOController.java
@@ -2,13 +2,19 @@ package com;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.SystemUtils;
 
 @RestController
 public class SSOController {
-	
-	 @GetMapping("/sso")
-	    public String viewHome() {
-	        return "hello";
-	    }
+
+	@GetMapping("/sso")
+	public String viewHome() {
+		return "hello";
+	}
+
+	@GetMapping("/apache")
+	public String getApache() {
+		return SystemUtils.USER_NAME;
+	}
 
 }
