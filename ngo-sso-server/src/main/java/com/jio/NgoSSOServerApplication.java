4697810d2fe35e3847298658941e47b647f2199b
sumansaurabh47 <sumansaurabh47@users.noreply.github.com>
2020-02-27 08:58:06 +0530

    Add files via upload

---
+++ ngo-sso-server/src/main/java/com/jio/NgoSSOServerApplication.java
@@ -0,0 +1,13 @@
+package com.jio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoSSOServerApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoSSOServerApplication.class, args);
+	}
+
+}
